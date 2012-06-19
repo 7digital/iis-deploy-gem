9fff8a497db3fc5e1970e6c4989c8011920b3c19
Henry Oswald <henry.oswald@7digital.com>
2012-06-19 16:19:54 +0100

    fully creates the website

--- spec/appcmd_wrapper_spec.rb
+++ spec/appcmd_wrapper_spec.rb
@@ -2,17 +2,16 @@ require 'rake'
 
 #SET appcmd="C:\windows\System32\inetsrv\appcmd.exe"
 
-#%appcmd% ADD SITE /name:%sitename% /bindings:"http://www.7digital.local:80" /physicalPath:"c:\work\sevendigital-com\src\SevenDigital.Com.Web"
-#%appcmd% SET SITE /site.name:%sitename% /applicationDefaults.applicationPool:%sitename%
 #C:\Program Files (x86)\IIS Resources\SelfSSL>selfssl.exe /T /N:CN=7digital.com /S:13 /Q
-
 
 
 class AppcmdWrapper
 	def Create(siteName, sitePath)
-		execute "%windir%\\system32\\inetsrc\\appcmd.exe DELETE SITE #{siteName}"
 		execute "%windir%\\system32\\inetsrc\\appcmd.exe DELETE APPPOOL #{siteName}"
+		execute "%windir%\\system32\\inetsrc\\appcmd.exe DELETE SITE #{siteName}"
 		execute "%windir%\\system32\\inetsrc\\appcmd.exe ADD APPPOOL /name:#{siteName} /managedRuntimeVersion:v4.0"
+		execute "%windir%\\system32\\inetsrc\\appcmd.exe ADD SITE /name:#{siteName} /physicalPath:#{sitePath}"
+		execute "%windir%\\system32\\inetsrc\\appcmd.exe SET SITE /site.name:#{siteName} /applicationDefaults.applicationPool:#{siteName}"
 	end
 
 	private
@@ -26,33 +25,39 @@ end
 require 'support/spec_helper'
 
 describe AppcmdWrapper do
-	context 'Given creating a new site' do	
+	context 'deploying a site to iis' do	
 		appcmdPath = "%windir%\\system32\\inetsrc\\appcmd.exe"
 		siteName = "7digital.com"
 		sitePath = "some/path/here"
 
-		before(:each) do
+		before(:each) do 
 			@appcmdWrapper = AppcmdWrapper.new
 		end
 		
-		it 'should tear down the old site' do
+		it 'tears down the old site' do
 			expect_shell_with_parameter("#{appcmdPath} DELETE SITE #{siteName}")
 			@appcmdWrapper.Create(siteName,"a/path")
 		end
 		
-		it 'should tear down the app pool' do
+		it 'tears down the app pool' do
 			expect_shell_with_parameter("#{appcmdPath} DELETE APPPOOL #{siteName}")
 			@appcmdWrapper.Create(siteName,"a/path")
 		end
 
-		it 'should create a new app pool' do
+		it 'creates a new app pool' do
 			expect_shell_with_parameter("#{appcmdPath} ADD APPPOOL /name:#{siteName} /managedRuntimeVersion:v4.0")
 			@appcmdWrapper.Create(siteName,"a/path")
 		end
 
-		it 'should create a new site' do
+		it 'creates a new site' do
 			expect_shell_with_parameter("#{appcmdPath} ADD SITE /name:#{siteName} /physicalPath:#{sitePath}")
-			@appcmdWrapper.Create(siteName,"a/path")
+			@appcmdWrapper.Create(siteName, sitePath)
+		end
+
+
+		it 'sets application pool' do
+			expect_shell_with_parameter("#{appcmdPath} SET SITE /site.name:#{siteName} /applicationDefaults.applicationPool:#{siteName}")
+			@appcmdWrapper.Create(siteName, sitePath)
 		end
 
 		private 
