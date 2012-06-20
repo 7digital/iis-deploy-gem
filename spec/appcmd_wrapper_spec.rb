ce1228714c33d9b16dc08f6a7f4e20e8b4010d72
Henry Oswald <henry.oswald@7digital.com>
2012-06-20 10:55:43 +0100

    seperated site builder and apppool builder

--- spec/appcmd_wrapper_spec.rb
+++ spec/appcmd_wrapper_spec.rb
@@ -3,14 +3,10 @@ require 'rake'
 #SET appcmd="C:\windows\System32\inetsrv\appcmd.exe"
 
 
-
-class AppcmdWrapper
+class IISSiteBuilder
 	def Create(siteName, sitePath)
-		execute "%windir%\\system32\\inetsrc\\appcmd.exe DELETE APPPOOL #{siteName}"
 		execute "%windir%\\system32\\inetsrc\\appcmd.exe DELETE SITE #{siteName}"
-		execute "%windir%\\system32\\inetsrc\\appcmd.exe ADD APPPOOL /name:#{siteName} /managedRuntimeVersion:v4.0"
 		execute "%windir%\\system32\\inetsrc\\appcmd.exe ADD SITE /name:#{siteName} /physicalPath:#{sitePath}"
-		execute "%windir%\\system32\\inetsrc\\appcmd.exe SET SITE /site.name:#{siteName} /applicationDefaults.applicationPool:#{siteName}"
 	end
 
 	def SelfSignSite(siteName)
@@ -24,12 +20,79 @@ class AppcmdWrapper
 end
 
 
+class IisAppPoolBuilder
+	@siteName
+	def initialize(siteName)
+		@siteName = siteName	
+	end
+
+	def create
+		execute "%windir%\\system32\\inetsrc\\appcmd.exe ADD APPPOOL /name:#{@siteName} /managedRuntimeVersion:v4.0"
+		self
+	end
+	
+	def delete
+		execute "%windir%\\system32\\inetsrc\\appcmd.exe DELETE APPPOOL #{@siteName}"
+		self
+	end
+
+	def assign
+		execute "%windir%\\system32\\inetsrc\\appcmd.exe SET SITE /site.name:#{@siteName} /applicationDefaults.applicationPool:#{@siteName}"
+		self
+	end
+
+	private
+	def execute(command)
+		sh command
+	end
+end
+
+class IISSelfSigning
+end
+
 require 'support/spec_helper'
 
-describe AppcmdWrapper do
+
+describe IisAppPoolBuilder do
+	siteName = "7digital.com"
+	appcmdPath = "%windir%\\system32\\inetsrc\\appcmd.exe"
 
 	before(:each) do 
-		@appcmdWrapper = AppcmdWrapper.new
+		@iisAppPoolBuilder = IisAppPoolBuilder.new(siteName)
+	end
+
+	context 'setting up app pool' do
+		it 'is fluent' do
+			@iisAppPoolBuilder.expects(:sh).with(includes(appcmdPath)).times 3
+			@iisAppPoolBuilder.delete().create().assign()
+		end
+
+		it 'creates a new app pool' do
+			expect_shell_with_parameter("#{appcmdPath} ADD APPPOOL /name:#{siteName} /managedRuntimeVersion:v4.0")
+			@iisAppPoolBuilder.create
+		end
+		it 'tears down the app pool' do
+			expect_shell_with_parameter("#{appcmdPath} DELETE APPPOOL #{siteName}")
+			@iisAppPoolBuilder.delete
+		end
+
+		it 'sets application pool' do
+			expect_shell_with_parameter("#{appcmdPath} SET SITE /site.name:#{siteName} /applicationDefaults.applicationPool:#{siteName}")
+			@iisAppPoolBuilder.assign
+		end
+	end
+
+	private
+	def expect_shell_with_parameter(parameter)
+		@iisAppPoolBuilder.expects(:sh).with(parameter).once
+		@iisAppPoolBuilder.stubs(:sh).with(Not(equals(parameter)))
+	end
+end
+
+describe IISSiteBuilder do
+
+	before(:each) do 
+		@iisSiteBuilder = IISSiteBuilder.new
 	end
 	
 	context 'self signing iis site' do
@@ -38,7 +101,7 @@ describe AppcmdWrapper do
 		
 		it 'get site info' do
 			expect_shell_with_parameter("#{selfSignPath} /T /N:CN=#{siteName} /S:13 /Q")
-			@appcmdWrapper.SelfSignSite(siteName)
+			@iisSiteBuilder.SelfSignSite(siteName)
 		end
 	end
 
@@ -47,37 +110,22 @@ describe AppcmdWrapper do
 		siteName = "7digital.com"
 		sitePath = "some/path/here"
 
-		
-		it 'tears down the old site' do
-			expect_shell_with_parameter("#{appcmdPath} DELETE SITE #{siteName}")
-			@appcmdWrapper.Create(siteName,"a/path")
-		end
-		
-		it 'tears down the app pool' do
-			expect_shell_with_parameter("#{appcmdPath} DELETE APPPOOL #{siteName}")
-			@appcmdWrapper.Create(siteName,"a/path")
-		end
-
-		it 'creates a new app pool' do
-			expect_shell_with_parameter("#{appcmdPath} ADD APPPOOL /name:#{siteName} /managedRuntimeVersion:v4.0")
-			@appcmdWrapper.Create(siteName,"a/path")
-		end
-
 		it 'creates a new site' do
 			expect_shell_with_parameter("#{appcmdPath} ADD SITE /name:#{siteName} /physicalPath:#{sitePath}")
-			@appcmdWrapper.Create(siteName, sitePath)
+			@iisSiteBuilder.Create(siteName, sitePath)
 		end
 
-
-		it 'sets application pool' do
-			expect_shell_with_parameter("#{appcmdPath} SET SITE /site.name:#{siteName} /applicationDefaults.applicationPool:#{siteName}")
-			@appcmdWrapper.Create(siteName, sitePath)
+		it 'tears down the old site' do
+			expect_shell_with_parameter("#{appcmdPath} DELETE SITE #{siteName}")
+			@iisSiteBuilder.Create(siteName,"a/path")
 		end
+		
+		
 	end
 	
 	private
 	def expect_shell_with_parameter(parameter)
-		@appcmdWrapper.expects(:sh).with(parameter).once
-		@appcmdWrapper.stubs(:sh).with(Not(equals(parameter)))
+		@iisSiteBuilder.expects(:sh).with(parameter).once
+		@iisSiteBuilder.stubs(:sh).with(Not(equals(parameter)))
 	end
 end
